f811072a9fb88017eb2dc88530a92a5b70732f72
Dmitriy Zhukov <zhukontime@gmail.com>
2026-07-01 03:44:35 +0400

    feat: better env; fix scripts

--- src/bot/main.ts
+++ src/bot/main.ts
@@ -1,9 +1,6 @@
-import { loadEnvFile } from "node:process";
-
 import { Bot } from "grammy";
 import { SoloMatch } from "../logic/RPSMatch.js";
 
-loadEnvFile();
 const TOKEN = process.env.BOT_TOKEN;
 if (!TOKEN) {
   throw new Error("BOT_TOKEN is not defined in the environment variables.");
@@ -19,4 +16,5 @@ const bot = new Bot(TOKEN);
 
 bot.on("message", (ctx) => ctx.reply("Bot is up!"));
 
+console.log("Bot is starting...");
 bot.start();
